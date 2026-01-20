710944e250742fa9f522847d41f6960b70ed6bb1
Sreenath Ganga <sreenath.sreeganga@gmail.com>
2026-01-20 22:32:11 +0530

    Update API_BASE_URL to new production endpoint

--- src/constants/API_ENDPOINTS.ts
+++ src/constants/API_ENDPOINTS.ts
@@ -1,6 +1,6 @@
 // src/constants/apiEndpoints.ts
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://sreenathganga-001-site12.jtempurl.com/api';
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://talkyapi.hestiatechnology.com/api';
 
 export const API_ENDPOINTS = {
   STAFF: {
